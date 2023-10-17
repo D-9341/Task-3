cd2d66d3e03b5d9456ce3741d4718e1755cd38af
Даниил <wsx86932321@yandex.ru>
2023-10-17 09:42:57 +0300

    task-3

---
+++ task-3.js
@@ -0,0 +1,9 @@
+const readlineSync = require('readline-sync');
+
+const calculateDiscount = (price, discountPercentage) => {
+    price = readlineSync.question('Введите цену: ')
+    discountPercentage = readlineSync.question('Введите процент скидки:')
+    return console.log(`Сумма с учётом скидки: ${price * (discountPercentage / 100)}`)
+}
+
+calculateDiscount()
