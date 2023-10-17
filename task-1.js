f92dcdac1373e2d68ebf0491099541b0fd9d795e
Даниил <wsx86932321@yandex.ru>
2023-10-17 09:27:05 +0300

    task-1

---
+++ task-1.js
@@ -0,0 +1,9 @@
+const readlineSync = require('readline-sync');
+
+const incomeTaxCalculator = (income) => {
+    income = readlineSync.question('Введите ваш доход: ')
+    tax = income * 0.15
+    return console.log(`Налог на доход равен ${tax}`)
+}
+
+incomeTaxCalculator()
