3a23d3af4d933f2a67ca16f32cf5e1510b53068a
Даниил <wsx86932321@yandex.ru>
2023-10-17 09:33:34 +0300

    task-2

---
+++ task-2.js
@@ -0,0 +1,9 @@
+const readlineSync = require('readline-sync');
+
+const celsiusToFahrenheit = (celsius) => {
+    celsius = readlineSync.question('Введите температуру по цельсию для конвертации: ')
+    fahrenheit = (celsius * (9/5) + 32)
+    return console.log(`Введённая температура по цельсию: ${celsius}\nКонвертированная температура по фаренгейту: ${fahrenheit}`)
+}
+
+celsiusToFahrenheit()
