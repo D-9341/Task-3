dc95f5c573733a84f3486e4b430d15645106b9b9
Даниил <wsx86932321@yandex.ru>
2023-10-17 10:03:28 +0300

    task-4

---
+++ task-4.js
@@ -0,0 +1,13 @@
+const readlineSync = require('readline-sync');
+
+const calculateRectangleProperties = (width, heigth) => {
+    width = readlineSync.question('Введите ширину: ');
+    width = Number(width);
+    heigth = readlineSync.question('Введите высоту: ');
+    heigth = Number(heigth);
+    P = width + heigth;
+    S = width * heigth;
+    return console.log(`Периметр введённого прямоугольника: ${P}\nПлощадь введённого прямоугольника: ${S}`);
+}
+
+calculateRectangleProperties()
